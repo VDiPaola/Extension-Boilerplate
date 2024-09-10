fb73e8b04cbca073afd9ad4edfe9e36776eabc3c
VDipaola <dipaolae98@gmail.com>
2024-09-11 00:39:33 +0100

    first

---
+++ src/content-scripts/main.js
@@ -0,0 +1,15 @@
+import { onCustomElementObserved } from "./classes/Helpers";
+import {Logger} from './Features/Logger'
+
+
+export class ChessCoach{
+
+    static async init (){
+        //wait for sidebar element
+        
+    }
+}
+
+window.addEventListener("load", async () => {
+    ChessCoach.init();
+})
